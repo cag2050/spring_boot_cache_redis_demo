07a6b715e2374803bfee8fcf32b4ca10a91e0615
chen_anguo <cag2050@163.com>
2019-08-19 20:08:19 +0800

    project init

---
+++ src/test/java/com/example/spring_boot_redis_demo/SpringBootRedisDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_boot_redis_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRedisDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
